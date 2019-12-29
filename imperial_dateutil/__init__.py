1896e950006af0ea7de0b35f201f65c9cd2a4990
Seonghyeon Kim <kim@seonghyeon.dev>
2019-12-29 14:31:27 +0900

    <bump>(comm): Release of 0.1.2
    The Emperor protects always and forever. He is the Omnissiah and will always live, always protect, always watch. He will protect all loyal to the Imperium and it is an honor to fight and die in the name of the emperor.

--- imperial_dateutil/__init__.py
+++ imperial_dateutil/__init__.py
@@ -1,4 +1,4 @@
 from imperial_dateutil.core.imperial import ImperialDatingSystem
 from imperial_dateutil.core.era_indomitus import EraIndomitusDatingSystem
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
